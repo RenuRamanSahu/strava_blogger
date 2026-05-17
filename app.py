5ed70d889fb6517ef18254bb286d79b85132a735
Renu Raman Sahu <38077212+RenuRamanSahu@users.noreply.github.com>
2026-05-17 12:00:01 +0530

    Implement Strava activity retrieval on webhook
    Added functionality to retrieve activity data from Strava API upon receiving a webhook event.

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI, Request
+import requests
+import os
 
 app = FastAPI()
-
 VERIFY_TOKEN = "strava_verify_secret"
+
+STRAVA_ACCESS_TOKEN = os.getenv("STRAVA_ACCESS_TOKEN")
+
+def get_activity(activity_id):
+    url = f"https://www.strava.com/api/v3/activities/{activity_id}"
+
+    headers = {
+        "Authorization": f"Bearer {STRAVA_ACCESS_TOKEN}"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    return response.json()
 
 @app.get("/webhook")
 async def verify(request: Request):
@@ -19,12 +33,30 @@ async def verify(request: Request):
     return {"error": "verification failed"}
 
 
+# @app.post("/webhook")
+# async def webhook(request: Request):
+#     data = await request.json()
+
+#     print("STRAVA EVENT RECEIVED:")
+#     print(data)
+
+#     return {"status": "received"}
+
 @app.post("/webhook")
 async def webhook(request: Request):
+
     data = await request.json()
 
-    print("STRAVA EVENT RECEIVED:")
-    print(data)
+    activity_id = data.get("object_id")
+    print("EVENT RECEIVED:", activity_id)
 
-    return {"status": "received"}
+    activity = get_activity(activity_id)
+    print("FULL ACTIVITY DATA:")
+    print(activity)
+
+    return {"status": "processed"}
+
+
+##################################################
+
 
